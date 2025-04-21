677e2f82c2b507af746e971dfda890177f70d86e
Usuario <Usuario@DESKTOP-KHUQE70>
2025-04-21 14:04:19 +0200

    Main

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,6 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos haciendo control de versiones con Git");
 		System.out.println("Despues de commit");
-		System.out.println("Mi ultimo comentrio");
+		System.out.println("Mi ultimo comentario");
 	}
 }
